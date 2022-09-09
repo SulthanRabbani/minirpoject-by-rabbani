d894eb98cb372e580c02e24cfe6f23f525bc10b3
Sulthan T. Rabbani <sulthanrabbanitaqi@gmail.com>
2022-09-09 15:11:04 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='h-[64px] border-b border-gray-300 justify-start items-center flex'>
+      <p className='pl-20 text-lg font-bold'>Product Roadmap</p>
+      <p>Testing</p>
+    </div>
+  );
+}
+
+export default App;
